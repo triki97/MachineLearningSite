7899f262a63ec2a5b35fb339d23bfed85fa0f036
triki97 <triki97@DESKTOP-MPO86SF>
2016-07-30 15:16:45 -0700

    Add project files.

---
+++ MachineLearningSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MachineLearningSite.MvcApplication" Language="C#" %>
---
+++ MachineLearningSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MachineLearningSite.Startup))]
+namespace MachineLearningSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
