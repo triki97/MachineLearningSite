a7f3625d187233d7142e8fc6552ef023ed509c8c
Ben Elliott <Ben Elliott>
2016-08-05 19:42:23 -0700

    mobile compatible responsiveness added

--- SplashSite/materialize/js/splash.js
+++ SplashSite/materialize/js/splash.js
@@ -8,3 +8,25 @@ function showContent(boxid){
     Materialize.fadeInImage('#card');
   }
 };
+
+function mobileViewUpdate() {
+    var viewportWidth = $(window).width();
+    if (viewportWidth < 450) {
+      $("#buttons").removeClass("row");
+      $("#choiceOne").addClass("container #description1");
+      $("#choiceTwo").addClass("container #description2");
+    }
+    else if (viewportWidth < 830 && viewportWidth > 450) {
+      $("#buttons").removeClass("row");
+      $("#choiceOne").addClass("container #description1");
+      $("#choiceTwo").addClass("container #description2");
+    }
+    else {
+      $("#buttons").addClass("row");
+      $("#choiceOne").removeClass("container #description1");
+      $("#choiceTwo").removeClass("container #description2");
+    }
+}
+
+$(window).load(mobileViewUpdate);
+$(window).resize(mobileViewUpdate);
